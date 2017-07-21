ec120ec4a52ef635d504e351edc9b79667c2d071
Kirk Tolleshaug <ktolleshaug@gmail.com>
2017-07-20 19:22:30 -0500

    add base type controller

---
+++ bbbs-2017-backend/Controllers/TypesController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace bbbs_2017_backend.Controllers
+{
+    [Route("api/[controller]")]
+    public class TypesController : Controller
+    {
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+    }
+
+}

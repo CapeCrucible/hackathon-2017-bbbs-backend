14640cd0672a9e0a29daa8dec30ed09beea46932
Kirk Tolleshaug <Kirk.Tolleshaug@vizientinc.com>
2017-07-21 10:38:03 -0500

    reup

--- AspNetCoreWebService/Startup.cs
+++ AspNetCoreWebService/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace bbbs2017backend
+namespace AspNetCoreWebService
 {
     public class Startup
     {

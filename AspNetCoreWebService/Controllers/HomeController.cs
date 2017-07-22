74ce0fefa806dc7bffff07df713ffe1a256dd38d
Kirk Tolleshaug <Kirk.Tolleshaug@vizientinc.com>
2017-07-21 23:31:50 -0500

    redo again

--- AspNetCoreWebService/Controllers/HomeController.cs
+++ AspNetCoreWebService/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace Bbbs2017Backend.Controllers
+namespace AspNetCoreWebService.Controllers
 {
     public class HomeController : Controller
     {
--- AspNetCoreWebService/Startup.cs
+++ AspNetCoreWebService/Startup.cs
@@ -8,7 +8,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-namespace Bbbs2017Backend
+namespace AspNetCoreWebService
 {
     public class Startup
     {

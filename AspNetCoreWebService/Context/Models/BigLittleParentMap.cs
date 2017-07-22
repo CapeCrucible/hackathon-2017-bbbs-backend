c8e1af5ba985e8e7250d7a914bdae8cc6a67daf0
DGEILE <decegeile@gmail.com>
2017-07-22 14:24:39 -0500

    Added models for little/parent and big/little/parent mappings

---
+++ AspNetCoreWebService/Context/Models/BigLittleParentMap.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Context.Models
+{
+    public class BigLittleParentMap
+    {
+        public int Id { get; set; }
+        public int LittleParentMapId { get; set; }
+        public virtual LittleParentMap LittleParentMap { get; set; }
+        public int BigId { get; set; }
+    }
+}
---
+++ AspNetCoreWebService/Context/Models/LittleParentMap.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Context.Models
+{
+    public class LittleParentMap
+    {
+        public int Id { get; set; }
+
+        public int LittleId { get; set; }
+        public virtual UserAccount Little { get; set; }
+
+        public int ParentId { get; set; }
+        public virtual UserAccount Parent { get; set; }
+    }
+}

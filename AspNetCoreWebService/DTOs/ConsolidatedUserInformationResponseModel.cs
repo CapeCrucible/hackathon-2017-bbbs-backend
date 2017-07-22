70249d7089c70a4a0df61d7738277df3448c3949
blake rhodes <brhodes@localhost.localdomain>
2017-07-22 18:53:42 -0500

    Create Response Model for Registering User

---
+++ AspNetCoreWebService/DTOs/ConsolidatedUserInformationResponseModel.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace AspNetCoreWebService.DTOs
+{
+    public class ConsolidatedUserInformationResponseModel
+    {
+        public UserAccountModel UserAccountModel;
+        public UserAddressModel UserAddressModel;
+        public ContactInfoModel ContactInfoModel;
+        public IEnumerable<InterestModel> InterestModels;
+    }
+}

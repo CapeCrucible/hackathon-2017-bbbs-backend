00011cb875eb34840fe2f99cf4651515746ab6b3
Kirk Tolleshaug <Kirk.Tolleshaug@vizientinc.com>
2017-07-22 03:28:47 -0500

    add automapper generics helper

---
+++ AspNetCoreWebService/Helpers/AutoMapperGenericsHelper.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+
+namespace Catalog.Common.Utilities
+{
+    /// <summary>
+    /// Uses AutoMammer nuget package to map EF <=> DTOs
+    /// </summary>
+    /// <typeparam name="TSource"></typeparam>
+    /// <typeparam name="TDestination"></typeparam>
+    public class AutoMapperGenericsHelper<TSource, TDestination>
+    {
+        public static TDestination Convert(TSource source)
+        {
+            Mapper.Initialize(cfg => cfg.CreateMap<TSource, TDestination>());
+            return Mapper.Map<TSource, TDestination>(source);
+        }
+    }
+}

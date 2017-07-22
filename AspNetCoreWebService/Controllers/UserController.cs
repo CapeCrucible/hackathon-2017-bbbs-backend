e526a60da90887819e3ff30188a2579e17fe1a1b
DGEILE <decegeile@gmail.com>
2017-07-22 12:32:27 -0500

    Expanded repo and service layer

---
+++ AspNetCoreWebService/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+﻿using AspNetCoreWebService.DTOs;
+using AspNetCoreWebService.Services;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Controllers
+{
+    public class UserController
+    {
+        [Route("api/[controller]")]
+        public class TypesController : Controller
+        {
+            // GET: api/values
+            [HttpGet]
+            [Route("GetAllUserTypes")]
+            public IEnumerable<UserTypeModel> GetAllUserTypes()
+            {
+                return TypeService.GetAllUserTypes();
+            }
+        }
+    }
+}
---
+++ AspNetCoreWebService/Services/ContactInfoService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AspNetCoreWebService.Services
+{
+    public class ContactInfoService
+    {
+    }
+}
